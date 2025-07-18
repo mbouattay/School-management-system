fdba26cccdc044a7b1d971dc6e7f869ab807864e
mahmoud bouattay <mahmoudbouattay178@gmail.com>
2025-07-18 13:08:56 +0100

    bulletin

--- src/bulletins/bulletins.controller.ts
+++ src/bulletins/bulletins.controller.ts
@@ -9,4 +9,14 @@ export class BulletinController{
     getBulletinForEtudiant(@Query('etudiantId') etudiantId: number, @Query('trimester') trimester: string) {
         return this.bulletinService.getBulletinForEtudiant(Number(etudiantId), trimester);
     }
+
+    @Get('by-classe')
+    getAllBulletinsByClasseAndTrimester(@Query('classeId') classeId: number, @Query('trimester') trimester: string) {
+        return this.bulletinService.getAllBulletinsByClasseAndTrimester(Number(classeId), trimester);
+    }
+
+    @Get('stats-moyenne')
+    getStatsMoyenneGeneraleByClasseAndTrimester(@Query('classeId') classeId: number, @Query('trimester') trimester: string) {
+        return this.bulletinService.getStatsMoyenneGeneraleByClasseAndTrimester(Number(classeId), trimester);
+    }
 }
